a002caa067d9633ca9b1300959fc8a990dc0de15
XXDIUX <162715717+XXDIUX@users.noreply.github.com>
2024-09-04 11:38:20 -0300

    Add files via upload

---
+++ .js
@@ -0,0 +1,12 @@
+// Programa utilizando o while loop
+const notas = [7, 8, 8, 9, 5];
+let soma = 0;
+let i = 0;
+
+while (i < notas.length) {
+    soma += notas[i];
+    i++;
+}
+
+const media = soma / notas.length;
+console.log("Média calculada com while loop:", media);
